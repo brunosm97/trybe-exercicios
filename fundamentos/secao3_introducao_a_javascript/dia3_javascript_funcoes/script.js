14d960abfc31271505ad60d90baeff3dd5e1d8e5
Bruno Santos <bs_machado97@hotmail.com>
2023-05-06 03:11:00 -0300

    atualiza exercicio de refatoração pra diminuir o codigo na função removeCliente

--- fundamentos/secao3_introducao_a_javascript/dia3_javascript_funcoes/script.js
+++ fundamentos/secao3_introducao_a_javascript/dia3_javascript_funcoes/script.js
@@ -23,23 +23,23 @@ console.log(divideSaldo(5)); // 20
 
 
 function sacar(valor, saldo) {
-    if (valor >= 1 && saldo >= 0) {
-        if (valor <= saldo) {
-            return "Saque realizado com sucesso. Novo saldo: R$ " + (saldo - valor) + ",00";
-        } else {
-            return "Saldo insuficiente. Seu saldo atual é: R$ " + saldo + ",00";
-        }
+  if (valor >= 1 && saldo >= 0) {
+    if (valor <= saldo) {
+      return "Saque realizado com sucesso. Novo saldo: R$ " + (saldo - valor) + ",00";
     } else {
-        return "Valor ou saldo inválido. O valor deve ser maior ou igual a 1 e saldo deve ser maior ou igual a 0.";
+      return "Saldo insuficiente. Seu saldo atual é: R$ " + saldo + ",00";
     }
+  } else {
+    return "Valor ou saldo inválido. O valor deve ser maior ou igual a 1 e saldo deve ser maior ou igual a 0.";
   }
-  
-  console.log(sacar(2, 20)); // Saque realizado com sucesso. Novo saldo: R$ 18,00
-  console.log(sacar(2, -5)); // Valor ou saldo inválido. O valor deve ser maior ou igual a 1 e saldo deve ser maior ou igual a 0.
-  console.log(sacar(2, 0)); // Saldo insuficiente. Seu saldo atual é: R$ 0,00
+}
+
+console.log(sacar(2, 20)); // Saque realizado com sucesso. Novo saldo: R$ 18,00
+console.log(sacar(2, -5)); // Valor ou saldo inválido. O valor deve ser maior ou igual a 1 e saldo deve ser maior ou igual a 0.
+console.log(sacar(2, 0)); // Saldo insuficiente. Seu saldo atual é: R$ 0,00
 
 
-  let status = 'deslogado'; // o status padrão é deslogado
+let status = 'deslogado'; // o status padrão é deslogado
 
 function logarDeslogar() { // aqui criamos uma função para verificar o status
   if (status === 'deslogado') { // se a pessoa usuária estiver deslogada, alteramos o status para logado
@@ -64,37 +64,63 @@ console.log('O usuário está ' + status + ' no sistema TrybeBank'); // O usuár
 let clientesTrybeBank = ['Ada', 'John', 'Gus'];
 
 function adicionarCliente(novoCliente) {
-    if(typeof novoCliente === 'string') {
-        clientesTrybeBank.push(novoCliente);
-        return "O cliente " + novoCliente + " foi adicionado ao array.";
-    }
-    else {
-        return 'O parâmetro passado deve ser do tipo "string"!';
-    }
+  if (typeof novoCliente === 'string') {
+    clientesTrybeBank.push(novoCliente);
+    return "O cliente " + novoCliente + " foi adicionado ao array.";
+  }
+  else {
+    return 'O parâmetro passado deve ser do tipo "string"!';
+  }
 }
 
-function excluiCliente(novoCliente) {
-    let clienteEncontrado = false;
-    if (typeof novoCliente === 'string') {
-        for (let index = 0; index < clientesTrybeBank.length; index++) {
-           if (novoCliente === clientesTrybeBank[index]) {
-                clientesTrybeBank.splice(index, 1);
-                clienteEncontrado = true;
-                return 'Cliente excluída(o) com sucesso.';
-            }
-        }
+
+//Refatorando a função excluiCliente
+function validaCliente(cliente) {
+  if (typeof cliente !== 'string') {
+    return 'O parâmetro passado deve ser do tipo "string"!';
+  } else {
+    return true;
+  }
+}
+
+function clienteIndex(cliente) {
+  for (let index = 0; index < clientesTrybeBank.length; index += 1) {
+    if (cliente === clientesTrybeBank[index]) {
+      return index;
     }
-    else {
-        return 'O parâmetro passado deve ser do tipo "string"!';
+  }
+  return false;
+}
+
+function clienteIndex(cliente) {
+  for (let index = 0; index < clientesTrybeBank.length; index += 1) {
+    if (cliente === clientesTrybeBank[index]) {
+      return index;
     }
-}  
+  }
+  return false;
+}
+
+function removeCliente(cliente) {
+  let validacao = validaCliente(cliente);
+  if (validacao !== true) {
+    return validacao;
+  }
+
+  let index = clienteIndex(cliente);
+  if (index === false) {
+    return 'Cliente não encontrada(o).'
+  }
+
+  clientesTrybeBank.splice(index, 1);
+  return 'Cliente excluída(o) com sucesso.';
+}
+
+console.log(removeCliente(false)); // O parâmetro passado deve ser do tipo "string"!
+console.log(removeCliente('Barney')); // Cliente não encontrada(o)
+console.log(removeCliente('John')); // Cliente excluída(o) com sucesso.
+console.log(clientesTrybeBank); // [ 'Ada', 'Gus' ]
 
 
-
-
-
-console.log(adicionarCliente('Bruno'));
-console.log(excluiCliente('Gus'));
-console.log(clientesTrybeBank);
 
 
